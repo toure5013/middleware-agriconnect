03d114fdf0b61ce68f093924660420bff1f6574c
SOULEYMANE TOURE <toure5013@gmail.com>
2019-12-18 07:42:52 +0000

    ALAHOUMA SOLI ALA MOUHALIMANA AL-BACHARIYA SOLAWATOU RABI ALEY-HI WA ALA ALI-HI WA SALIM

--- index.js
+++ index.js
@@ -2,6 +2,44 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
+
+
+const allowsOrigins = [
+    'capacitor://localhost',
+    'ionic:localhost',
+    'http://localhost',
+    'http://localhost:8080',
+    'http://localhost:8100',
+]
+
+const corsOptions = {
+
+    origin: (origin, callback) => {
+        if (allowsOrigins.includes(origin) || !origin) {
+            callback(null, true)
+        } else {
+            callback(new Error('Origin not allowed by Cors'));
+        }
+    }
+}
+
+app.options('*', cors(corsOptions))
+
+
+const public = "./public";
+
+//Static file 
+app.use(express.static(public));
+
+
+
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", '*');
+    res.setHeader("Access-Control-Allow-Headers", '*');
+    next();
+})
+
+
 const port = process.env.PORT || 8083
 
 app.get("/", (req, res)=>{
